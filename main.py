eeb71d4d728d5720298885ea919d40541156f25d
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-04-01 15:10:48 +0200

    Removed restore cookies, now login is done via selenium

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ driver = Chrome()
 
 rewards = msrewards.MicrosoftRewards(driver)
 
-rewards.go_to_home()
-rewards.restore_cookies()
+# rewards.go_to("https://www.google.com")
+# rewards.restore_cookies()
 rewards.go_to_home()
 
-rewards.execute_activities()
+rewards.execute_activities(rewards.get_todo_activities())
 
 print()
