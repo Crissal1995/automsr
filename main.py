d66f99bd005429b17d8735f859fdf295e0e0da71
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-04-02 13:06:35 +0200

    Added possibility to skip activities

--- main.py
+++ main.py
@@ -8,10 +8,55 @@ logger.addHandler(logging.StreamHandler())
 logger.addHandler(logging.FileHandler("main.log", "w"))
 logger.setLevel(logging.INFO)
 
+
+def to_skip(creds: dict):
+    """Function utility to know which activity,
+    listed as (daily_activities, daily_searches), is
+    to be skipped.
+    False means the activity should not be skipped,
+    True otherwise.
+    """
+    if not creds.get("skip"):
+        return False, False
+
+    # lower() to ensure it's a str
+    try:
+        skip_str = creds["skip"].lower()
+    except AttributeError:
+        err = "skip value must be string!"
+        logging.error(err)
+        raise ValueError(err)
+
+    values = ("activity", "search", "all")
+    if skip_str not in values:
+        err = f"skip string must be in {values}"
+        logging.error(err)
+        raise ValueError(err)
+
+    return_dict = {
+        "activity": (True, False),
+        "search": (False, True),
+        "all": (True, True),
+    }
+    return return_dict[skip_str]
+
+
 with open("credentials.json") as fp:
     credentials_list = json.load(fp)
 
 for i, credentials in enumerate(credentials_list):
     logging.info(f"Working on credentials no. {i + 1}")
-    # MicrosoftRewards.daily_activities(credentials=credentials)
-    MicrosoftRewards.daily_searches(credentials=credentials)
+
+    skip_activity, skip_searches = to_skip(credentials)
+
+    if not skip_activity:
+        logging.info("Start daily activities")
+        MicrosoftRewards.daily_activities(credentials=credentials)
+    else:
+        logging.info("Skipping daily activities")
+
+    if not skip_searches:
+        logging.info("Start daily searches")
+        MicrosoftRewards.daily_searches(credentials=credentials)
+    else:
+        logging.info("Skipping daily searches")
