39f55fd06da449a7ce0b70a6a5c6414053847e99
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-05-02 12:52:00 +0200

    Added query_gen to Takeout Search Generator

--- msrewards/search.py
+++ msrewards/search.py
@@ -1,12 +1,27 @@
+import json
 import logging
+import pathlib
 import random
+import re
 import string
 from abc import ABC
 from typing import Generator
+from urllib.parse import urlparse
 
 from selenium.webdriver.common.keys import Keys
 
+from msrewards.utility import config
+
 logger = logging.getLogger(__name__)
+
+
+def is_valid_url(url: str):
+    try:
+        result = urlparse(url)
+    except (AttributeError, ValueError):
+        return False
+    else:
+        return all(field for field in (result.scheme, result.netloc, result.path))
 
 
 class SearchGenerator(ABC):
@@ -50,4 +65,32 @@ class GoogleTakeoutSearchGenerator(SearchGenerator):
         return random.randint(10, 60)
 
     def query_gen(self) -> Generator:
-        ...
+        while True:
+            # get a random activity from the activities
+            activity = random.choice(self.activities)
+
+            # parse query from url
+            title_url_query = urlparse(activity["titleUrl"]).query.replace("q=", "")
+            logger.debug(f"url query is {title_url_query}")
+
+            # yield the correct value, based on the validity of title url query
+            if is_valid_url(title_url_query):
+                yield re.sub("&usg=[a-zA-Z0-9]+", "", title_url_query)
+            else:
+                yield title_url_query.replace("+", " ")
+
+    def __init__(self):
+        super().__init__()
+        takeout_json = pathlib.Path(config["automsr"]["takeout"])
+        if not takeout_json.exists():
+            msg = f"Takeout json file doesn't exist! Path provided: {takeout_json}"
+            logger.error(msg)
+            raise FileNotFoundError(msg)
+
+        with open(takeout_json) as f:
+            self.activities = json.load(f)
+
+        if not self.activities:
+            msg = "No activity found inside the file provided!"
+            logger.error(msg)
+            raise ValueError(msg)
