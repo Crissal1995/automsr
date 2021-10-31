e3115c12cd5a3260ce34befb721924d0112b29aa
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-10-31 16:20:35 +0100

    Added more States to store in DB
    - State now is the abstract class
    - PointsState is the new classname for storing points
    - ExecutionState and ActivityState are added for both execution metadata and activity metadata
    - New methods are added to StateManager

--- msrewards/state.py
+++ msrewards/state.py
@@ -1,24 +1,33 @@
+import datetime
+import enum
+import hashlib
 import sqlite3
+from abc import ABC
 from dataclasses import astuple, dataclass
-from typing import List, Optional, Sequence
+from typing import Any, List, Optional, Sequence
 
 DB_NAME = "automsr.sqlite"
 
-DB_TABLE = "states"
-DB_SQL_INIT = (
-    f"create table if not exists {DB_TABLE} "
-    "(email text not null, "
-    "points int not null, "
-    "timestamp integer not null, "
-    "points_delta int not null)"
-)
-DB_SQL_INSERT = f"insert into {DB_TABLE} values (?, ?, ?, ?)"
-DB_SQL_QUERY_ALL = f"select * from {DB_TABLE}"
-DB_SQL_QUERY_EMAIL = f"select * from {DB_TABLE} t where t.email = ?"
+
+# https://stackoverflow.com/questions/60590442
+@dataclass
+class AbstractDataclass(ABC):
+    def __new__(cls, *args, **kwargs):
+        if cls == AbstractDataclass or cls.__bases__[0] == AbstractDataclass:
+            raise TypeError("Cannot instantiate abstract class.")
+        return super().__new__(cls)
 
 
-@dataclass()
-class State:
+class State(AbstractDataclass):
+    DB_TABLE: str
+    DB_SQL_INIT: str
+    DB_SQL_INSERT: str
+    DB_SQL_QUERY_ALL: str
+    DB_SQL_QUERY_EMAIL: str
+
+
+@dataclass
+class PointsState(State):
     # the email of the account owner
     email: str
 
@@ -30,36 +39,168 @@ class State:
     timestamp: int
 
     # points obtained in this run
-    points_delta: Optional[int]
+    points_delta: Optional[int] = None
+
+    # override attributes
+    DB_TABLE = "points"
+    DB_SQL_INIT = (
+        f"create table if not exists {DB_TABLE} "
+        "(email text not null, "
+        "points int not null, "
+        "timestamp integer not null, "
+        "points_delta int)"
+    )
+    DB_SQL_INSERT = f"insert into {DB_TABLE} values (?, ?, ?, ?)"
+    DB_SQL_QUERY_ALL = f"select * from {DB_TABLE}"
+    DB_SQL_QUERY_EMAIL = f"select * from {DB_TABLE} t where t.email = ?"
+
+
+@dataclass
+class ExecutionState(State):
+    email: str
+    timestamp: int
+    activities_count: int
+
+    # override attributes
+    DB_TABLE = "execution"
+    DB_SQL_INIT = (
+        f"create table if not exists {DB_TABLE} "
+        "(email text not null, "
+        "timestamp integer not null, "
+        "activities_count int not null)"
+    )
+    DB_SQL_INSERT = f"insert into {DB_TABLE} values (?, ?, ?)"
+    DB_SQL_QUERY_ALL = f"select * from {DB_TABLE}"
+    DB_SQL_QUERY_EMAIL = f"select * from {DB_TABLE} t where t.email = ?"
+
+
+class ActivityStatusEnum(enum.Enum):
+    TODO = "TODO"
+    DONE = "DONE"
+    INVALID = "INVALID"
+
+
+@dataclass
+class ActivityState(State):
+    email: str
+    timestamp: int
+    daily: bool
+    status: str
+    title: Optional[str] = None
+    description: Optional[str] = None
+    hash: Optional[str] = None
+
+    def __post_init__(self):
+        # raises ValueError if an invalid string is provided
+        self.status = self.status.upper()
+        ActivityStatusEnum(self.status)
+
+        # create hash with MD5 if not provided
+        if not self.hash:
+            date = datetime.date.fromtimestamp(self.timestamp)
+            attrs = (self.email, date, self.daily, self.title, self.description)
+            hashstr = b"".join(bytes(str(x), "utf-8") for x in attrs)
+            self.hash = hashlib.md5(hashstr).hexdigest()
+
+    # override attributes
+    DB_TABLE = "activity"
+    DB_SQL_INIT = (
+        f"create table if not exists {DB_TABLE} "
+        "(email text not null, "
+        "timestamp integer not null, "
+        "daily bool not null, "
+        "status text not null, "
+        "title text, "
+        "description text,"
+        "hash text)"
+    )
+    DB_SQL_INSERT = f"insert into {DB_TABLE} values (?, ?, ?, ?, ?, ?, ?)"
+    DB_SQL_QUERY_ALL = f"select * from {DB_TABLE}"
+    DB_SQL_QUERY_EMAIL = f"select * from {DB_TABLE} t where t.email = ?"
+    DB_SQL_QUERY_HASH = f"select * from {DB_TABLE} t where t.hash = ?"
+    DB_SQL_UPDATE_HASH = f"update {DB_TABLE} set status = ? where hash = ?"
+
+
+@dataclass
+class StateFilter:
+    query: str
+    values: List[Any]
+
+    def execute(self, cursor: sqlite3.Cursor):
+        cursor.execute(self.query, self.values)
 
 
 class StateManager:
+    state_classes_map = {
+        "points": PointsState,
+        "execution": ExecutionState,
+        "activity": ActivityState,
+    }
+    state_classes = tuple(state_classes_map.values())
+
     def __init__(self):
         self.conn = sqlite3.connect(DB_NAME)
         self.cursor = self.conn.cursor()
 
-        self.cursor.execute(DB_SQL_INIT)
-        self.conn.commit()
+        for stateclass in self.state_classes:
+            self.cursor.execute(stateclass.DB_SQL_INIT)
+            self.conn.commit()
 
     def insert_state(self, state: State, commit: bool = True):
-        self.cursor.execute(DB_SQL_INSERT, astuple(state))
+        self.cursor.execute(state.DB_SQL_INSERT, astuple(state))
         if commit:
             self.conn.commit()
 
-    def insert_states(self, states: Sequence[State]):
+    def insert_states(self, states: Sequence[State], commit: bool = True):
         for state in states:
             self.insert_state(state, commit=False)
-        self.conn.commit()
+        if commit:
+            self.conn.commit()
 
-    def fetch_states_filtered(self, email: str) -> List[State]:
-        self.cursor.execute(DB_SQL_QUERY_EMAIL, (email,))
-        return [State(*t) for t in self.cursor.fetchall()]
+    def update_states_filter_hash(
+        self, kind: str, hashstr: str, new_value: str, commit: bool = True
+    ):
+        kind_cls = self._get_kind_state(kind)
+        query = kind_cls.DB_SQL_UPDATE_HASH
+        values = [new_value, hashstr]
+        self.cursor.execute(query, values)
+        if commit:
+            self.conn.commit()
 
-    def fetch_states(self) -> List[State]:
-        self.cursor.execute(DB_SQL_QUERY_ALL)
-        return [State(*t) for t in self.cursor.fetchall()]
+    @classmethod
+    def _get_kind_state(cls, kind: str):
+        kind = kind.lower()
+        kind_cls = cls.state_classes_map.get(kind)
+        if not kind_cls:
+            keys = list(cls.state_classes_map.keys())
+            raise ValueError(f"Invalid state kind provided! Availables are: {keys}")
+        return kind_cls
 
-    def _raw_query(self, query: str):
+    def fetch_states_filter(self, kind: str, sfilter: StateFilter) -> List[State]:
+        kind_cls = self._get_kind_state(kind)
+        sfilter.execute(cursor=self.cursor)
+        return [kind_cls(*t) for t in self.cursor.fetchall()]
+
+    def fetch_states_filter_email(self, kind: str, email: str) -> List[State]:
+        kind_cls = self._get_kind_state(kind)
+        query = kind_cls.DB_SQL_QUERY_EMAIL
+        values = [email]
+        sfilter = StateFilter(query, values)
+        return self.fetch_states_filter(kind, sfilter)
+
+    def fetch_states_filter_hash(self, kind: str, hashstr: str) -> List[State]:
+        kind_cls = self._get_kind_state(kind)
+        query = kind_cls.DB_SQL_QUERY_HASH
+        values = [hashstr]
+        sfilter = StateFilter(query, values)
+        return self.fetch_states_filter(kind, sfilter)
+
+    def fetch_states(self, kind: str) -> List[State]:
+        kind_cls = self._get_kind_state(kind)
+        self.cursor.execute(kind_cls.DB_SQL_QUERY_ALL)
+        return [kind_cls(*t) for t in self.cursor.fetchall()]
+
+    def _raw_query(self, query: str) -> Any:
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
