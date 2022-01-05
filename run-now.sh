e595f3bd55bddc833b0c85f698c1d75edf986b0d
Cristiano Salerno <cristianosalerno1@gmail.com>
2022-01-05 10:17:22 +0000

    Removed run-now.sh (use run.sh 0) + Look for common venv names

--- run-now.sh
+++
@@ -1,9 +0,0 @@
-echo "**********************************"
-echo "******** STARTING AUTOMSR ********"
-echo "**********************************"
-
-date
-TS=$(date +%F)
-
-mkdir -p logs
-venv/bin/python main.py $@ 2>&1 | tee -a "logs/automsr-$TS.log"
--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ MAX_HOURS_TO_DELAY=2
 
 DELAY_TIME_M=$((1 + $RANDOM % ($MAX_HOURS_TO_DELAY * 60)))
 DELAY_TIME_S=$(($DELAY_TIME_M * 60))
-TS=$(date +%d-%m-%Y_%k-%M)
+TS=$(date +%F_%R)
 
 AUTOMSR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -16,21 +16,26 @@ if [ $# -gt 0 ]
 then
 	echo "Time provided: $1"
 	DELAY_TIME_S=$1
+	DELAY_TIME_M=$(($DELAY_TIME_S / 60))
 fi
+
+REMAINDER=$(($DELAY_TIME_S % 60))
+echo "[$(date)] Wait $DELAY_TIME_M min, $REMAINDER sec before AutoMSR starts"
+
+sleep $DELAY_TIME_S
+echo "[$(date)] Ready to start AutoMSR"
 
 cd ${AUTOMSR_PATH}
 
-echo "[$(date)] Wait $DELAY_TIME_M minutes before run automsr"
-
-sleep $DELAY_TIME_S
-
-echo "[$(date)] Ready to start automsr"
-
-source venv/bin/activate
+possible_venvs=( ".venv" "venv" "virtualenv" )
+for venv in "${possible_venvs[@]}"
+do
+	if [[ -f "$venv/bin/activate" ]]; then
+		echo "Sourcing $venv/bin/activate"
+		source "$venv/bin/activate"
+		break
+	fi
+done
 
 mkdir -p logs
-
-DISPLAY=:0 python3 main.py  2>&1 | tee -a "logs/automsr-$TS.log"
- 
-#sudo poweroff -f
-
+DISPLAY=:0 python3 main.py 2>&1 | tee -a "logs/automsr-$TS.log"
