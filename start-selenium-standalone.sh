97967512a8b2077d32af7f9f8aa1a29fc1b3910b
Cristiano Salerno <cristianosalerno1@gmail.com>
2022-04-25 11:20:12 +0200

    FIX: - switched from Selenium Chrome Node built with Dockerfile.chrome to Selenium Standalone Chrome image

--- start-selenium-standalone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# IMPORTANT: Change this file only in directory Standalone!
-
-java ${JAVA_OPTS} -cp ${JAVA_CLASSPATH:-"/opt/selenium/*:."} org.openqa.grid.selenium.GridLauncherV3 \
-    ${SE_OPTS}
