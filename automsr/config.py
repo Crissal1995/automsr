a3ce0b68b54a25e8249090ca4d292ca91dbfee23
Cristiano Salerno <cristianosalerno1@gmail.com>
2023-08-06 15:41:08 +0200

    add another constraint improved doctest

--- automsr/config.py
+++ automsr/config.py
@@ -1,9 +1,9 @@
 import json
 from pathlib import Path
-from typing import Any, Collection, Dict, Union
+from typing import Any, Dict, List, Optional, Union
 
 import yaml
-from email_validator import validate_email, ValidatedEmail
+from email_validator import ValidatedEmail, validate_email
 from pydantic import BaseModel, ConfigDict, field_validator
 
 from automsr.datatypes import RewardsType
@@ -11,24 +11,26 @@ from automsr.datatypes import RewardsType
 
 class AutomsrConfig(BaseModel):
     credentials: Path
-    skip: Union[RewardsType, Collection[RewardsType]]
+    skip: Union[None, RewardsType, List[RewardsType]] = None
 
 
 class EmailConfig(BaseModel):
-    enable: bool
-    sender: str
-    recipient: str
+    enable: bool = False
+    sender: Optional[str] = None
+    recipient: Optional[str] = None
 
     @field_validator("sender", "recipient")
     @classmethod
-    def _validate_email(cls, value: str) -> str:
+    def _validate_email(cls, value: Optional[str]) -> Optional[str]:
+        if value is None:
+            return None
         validated_email: ValidatedEmail = validate_email(value)
         return validated_email.normalized
 
 
 class SeleniumConfig(BaseModel):
+    chrome_path: Path
     chromedriver_path: Path
-    chrome_binary_path: Path
 
 
 class Config(BaseModel):
@@ -36,7 +38,15 @@ class Config(BaseModel):
 
     version: str = "v1"
     automsr: AutomsrConfig
+    email: EmailConfig
     selenium: SeleniumConfig
+
+    @field_validator("version")
+    @classmethod
+    def _assert_version(cls, value: str) -> str:
+        if value != "v1":
+            raise ValueError("Only version 'v1' is supported!")
+        return value
 
     @classmethod
     def from_yaml(cls, path: Path) -> "Config":
@@ -45,7 +55,7 @@ class Config(BaseModel):
         """
 
         data = yaml.safe_load(open(path))
-        return cls(**data)
+        return cls.from_dict(data)
 
     @classmethod
     def from_json(cls, path: Path) -> "Config":
@@ -54,16 +64,28 @@ class Config(BaseModel):
         """
 
         data = json.load(open(path))
-        return cls(**data)
+        return cls.from_dict(data)
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "Config":
         """
         Load config from an in-memory mapping.
 
-        >>> _data = {"automsr": {}, "selenium": {"foo": "bar"}}
+        >>> _data = {
+        ...     "automsr": {"credentials": Path("creds.json")},
+        ...     "email": {},
+        ...     "selenium": {"chrome_path": Path("chrome.exe"), "chromedriver_path": Path("chromedriver.exe")},
+        ... }
         >>> Config.from_dict(_data)  # doctest: +ELLIPSIS
-        Config(version='v1', automsr=..., selenium=...)
+        Config(version='v1', automsr=..., email=..., selenium=...)
+        >>> _data["version"] = "v2"
+        >>> Config.from_dict(_data)  # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        ...
+        pydantic_core._pydantic_core.ValidationError: 1 validation error for Config
+        version
+          Value error, Only version 'v1' is supported! [type=value_error, input_value='v2', input_type=str]
+        ...
         """
 
         return cls(**data)
