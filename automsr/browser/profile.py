73cf634b46c738d6017320491a5cda3f71b15fba
Cristiano Salerno <cristianosalerno1@gmail.com>
2023-08-23 22:37:00 +0200

    CLI Rework (#27)
    - CLI reworked, now there are two subcommands available: `run` and
    `profiles`.
    - `automsr` won't execute anything but display the help and then exit
    with 1.

--- automsr/browser/profile.py
+++ automsr/browser/profile.py
@@ -2,9 +2,9 @@ import json
 import logging
 import os
 import sys
-from enum import Enum
+from enum import Enum, auto
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from attrs import define
 
@@ -13,124 +13,169 @@ logger = logging.getLogger(__name__)
 ENV_HOME = os.environ.get("HOME")
 ENV_LOCALAPPDATA = os.environ.get("LOCALAPPDATA")
 
-CHROME_PROFILES_LOCATIONS: Dict[str, List[str]] = {
-    "macOS": [
-        f"{ENV_HOME}/Library/Application Support/Google/Chrome",
-        f"{ENV_HOME}/Library/Application Support/Google/Chrome Canary",
-        f"{ENV_HOME}/Library/Application Support/Chromium",
-    ],
-    "windows": [
-        f"{ENV_LOCALAPPDATA}\\Google\\Chrome\\User Data",
-        f"{ENV_LOCALAPPDATA}\\Google\\Chrome SxS\\User Data",
-        f"{ENV_LOCALAPPDATA}\\Chromium\\User Data",
-    ],
-    "linux": [
-        f"{ENV_HOME}/.config/google-chrome",
-        f"{ENV_HOME}/.config/google-chrome-beta",
-        f"{ENV_HOME}/.config/chromium",
-    ],
-}
-
 
 class ChromeVariant(Enum):
     """
     Chrome variants available
     """
 
-    CHROME = 0
-    CHROME_CANARY = 1
-    CHROMIUM = 2
+    CHROME = auto()
+    CHROME_CANARY = auto()
+    CHROMIUM = auto()
+
+
+class OutputFormat(Enum):
+    """
+    Types of output formats available for printing found Chrome profiles o stdout.
+    """
+
+    LIST = "list"
+    JSON = "json"
+    PRETTY_JSON = "pretty-json"
+
+
+def get_platform() -> str:
+    """
+    Returns a simplified version of the system platform in use.
+    """
+
+    retval = "linux"  # assumes linux env if not win32 or darwin
+
+    if sys.platform.startswith("win32"):
+        retval = "windows"
+    elif sys.platform.startswith("darwin"):
+        retval = "macOS"
+
+    return retval
 
 
 @define
 class ChromeProfile:
-    name: str
+    displayed_name: str
     path: Path
 
+    RESERVED_NAMES = ["System Profile", "Guest Profile"]
+
     def as_dict(self) -> Dict[str, str]:
-        return dict(name=self.name, path=str(self.path.resolve()))
+        return dict(displayed_name=self.displayed_name, path=str(self.path.resolve()))
 
+    @classmethod
+    def from_directory(cls, path: Path) -> Optional["ChromeProfile"]:
+        """
+        Returns a Chrome Profile based on a directory.
 
-def get_chrome_profile_location(variant: ChromeVariant) -> Path:
-    """
-    Returns Chrome profiles' path associated with current platform and selected variant
-    """
+        Returns `None` if the provided `path` is not a valid Chrome Profile.
+        """
 
-    key = "linux"  # assumes linux env if not win32 or darwin
+        # If the provided path is not an existing directory, returns None
+        if not path.is_dir():
+            return None
 
-    if sys.platform.startswith("win32"):
-        key = "windows"
-    elif sys.platform.startswith("darwin"):
-        key = "macOS"
+        # If the directory name is one of the reserved name for Google profiles
+        # not matching any user profile, returns None
+        if path.name in cls.RESERVED_NAMES:
+            return None
 
-    return Path(CHROME_PROFILES_LOCATIONS[key][variant.value])
-
-
-def get_all_chrome_profile_locations() -> Dict[ChromeVariant, Path]:
-    """
-    Returns a list of all available Chrome profiles' locations, trying every ChromeVariant.
-    """
-
-    paths = {}
-
-    for name, value in ChromeVariant.__members__.items():
-        path = get_chrome_profile_location(variant=value)
-        if path.exists():
-            logger.info(f"Chrome variant found: {name}")
-            logger.debug(f"Chrome variant profiles path: {path}")
-            paths[value] = path
-
-    if not paths:
-        err = "No default location path for Chrome profiles was found!"
-        raise FileNotFoundError(err)
-    else:
-        return paths
-
-
-def parse_profiles(userdata_path: Path) -> List[ChromeProfile]:
-    """
-    Get a list of Chrome profiles, starting from the root user data path.
-    """
-
-    profiles = []
-
-    for dir_ in userdata_path.iterdir():
-        # if it's not a folder, surely it's not a profile
-        if not dir_.is_dir():
-            continue
-
-        # if current profile is one of the blacklisted profiles, continue
-        blacklisted_profiles = ("System Profile", "Guest Profile")
-        if dir_.stem in blacklisted_profiles:
-            continue
-
-        # if this file doesn't exist, it's not a profile
-        preferences_path = dir_ / "Preferences"
+        # If this file doesn't exist, it's not a profile
+        preferences_path = path / "Preferences"
         if not preferences_path.is_file():
-            continue
+            return None
 
         preferences_dict = json.load(open(preferences_path, encoding="utf-8"))
-        name = preferences_dict.get("profile", {}).get("name", "")
-        profile = ChromeProfile(
-            name=name,
-            path=dir_,
+        displayed_name = preferences_dict.get("profile", {}).get("name", "")
+        return cls(displayed_name=displayed_name, path=path)
+
+
+@define
+class ProfilesExecutor:
+    chrome_variant: ChromeVariant = ChromeVariant.CHROME
+    profiles_root_path: Optional[Path] = None
+
+    CHROME_PROFILES_LOCATIONS = {
+        "macOS": {
+            ChromeVariant.CHROME: f"{ENV_HOME}/Library/Application Support/Google/Chrome",
+            ChromeVariant.CHROME_CANARY: f"{ENV_HOME}/Library/Application Support/Google/Chrome",
+            ChromeVariant.CHROMIUM: f"{ENV_HOME}/Library/Application Support/Google/Chrome",
+        },
+        "windows": {
+            ChromeVariant.CHROME: f"{ENV_LOCALAPPDATA}\\Google\\Chrome\\User Data",
+            ChromeVariant.CHROME_CANARY: f"{ENV_LOCALAPPDATA}\\Google\\Chrome SxS\\User Data",
+            ChromeVariant.CHROMIUM: f"{ENV_HOME}/Library/Application Support/Chromium",
+        },
+        "linux": {
+            ChromeVariant.CHROME: f"{ENV_HOME}/.config/google-chrome",
+            ChromeVariant.CHROME_CANARY: f"{ENV_HOME}/.config/google-chrome-beta",
+            ChromeVariant.CHROMIUM: f"{ENV_HOME}/.config/chromium",
+        },
+    }
+
+    def get_profiles_root_path(self) -> Path:
+        """
+        Based on current state, get the root path of Chrome profiles.
+        """
+
+        if self.profiles_root_path is not None:
+            logger.info(
+                "Profiles root path manually set to: %s", self.profiles_root_path
+            )
+            return self.profiles_root_path
+
+        platform = get_platform()
+        logger.debug("Platform to use: %s", platform)
+        logger.debug("Chrome variant to use: %s", self.chrome_variant)
+
+        profiles_root_path_str = self.CHROME_PROFILES_LOCATIONS[platform][
+            self.chrome_variant
+        ]
+        profiles_root_path = Path(profiles_root_path_str)
+        logger.info(
+            "Profiles root path automatically found to be: %s", profiles_root_path
         )
-        profiles.append(profile)
+        return profiles_root_path
 
-    return profiles
+    def get_profiles(self) -> List[ChromeProfile]:
+        """
+        Return the list of Chrome profiles found.
+        """
 
+        root_path = self.get_profiles_root_path()
 
-def print_profiles() -> None:
-    """
-    Retrieve Chrome profiles for all Chrome variants found and print them to stdout.
-    """
+        # Safety check
+        if not root_path.exists():
+            raise FileNotFoundError(root_path)
 
-    all_paths = get_all_chrome_profile_locations()
-    all_profiles_as_dict = []
-    for variant, rootpath in all_paths.items():
-        profiles = parse_profiles(userdata_path=rootpath)
+        chrome_profiles: List[ChromeProfile] = []
 
-        logger.info("Found %s Chrome profiles for variant %s", len(profiles), variant)
-        all_profiles_as_dict.extend([profile.as_dict() for profile in profiles])
+        for directory in root_path.iterdir():
+            chrome_profile = ChromeProfile.from_directory(path=directory)
+            if chrome_profile is None:
+                continue
 
-    print(json.dumps(all_profiles_as_dict))
+            logger.info("Found Chrome profile: %s", chrome_profile)
+            chrome_profiles.append(chrome_profile)
+
+        return chrome_profiles
+
+    def print_profiles(self, output_format: OutputFormat = OutputFormat.LIST) -> None:
+        """
+        Get the list of Chrome profiles found, then print it to stdout.
+
+        It's possible to specify a type for printing the values obtained to stdout.
+        """
+
+        profiles: List[ChromeProfile] = self.get_profiles()
+
+        if output_format is OutputFormat.LIST:
+            data = "\n".join([str(profile) for profile in profiles])
+
+        elif output_format is OutputFormat.JSON:
+            data = json.dumps([profile.as_dict() for profile in profiles])
+
+        elif output_format is OutputFormat.PRETTY_JSON:
+            data = json.dumps([profile.as_dict() for profile in profiles], indent=4)
+
+        else:
+            raise ValueError(output_format)
+
+        data += "\n"  # add manually a final newline
+        sys.stdout.write(data)
--- automsr/cli.py
+++ automsr/cli.py
@@ -1,9 +1,12 @@
 import logging
+import sys
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Any, Callable
 
-from attr import define
+from attr import define, field
 
+from automsr.browser.profile import OutputFormat, ProfilesExecutor
 from automsr.config import Config
 from automsr.executor import MultipleTargetsExecutor
 
@@ -14,19 +17,55 @@ DEFAULT_CONFIG_PATH = Path("config.yaml")
 
 @define
 class Args:
+    """
+    Class mapping of CLI arguments.
+    """
+
+    # Method to invoke with a specific subparser
+    func: Callable[["Args"], Any]
+
+    # Common args
     config: Path = DEFAULT_CONFIG_PATH
     verbose: bool = False
 
+    # Run args
 
-def main(args: Args) -> None:
+    # Profiles args
+    format: OutputFormat = field(default=OutputFormat.LIST, converter=OutputFormat)
+
+
+def run(args: Args) -> None:
+    """
+    Method to invoke when `run` is executed.
+    """
+
     config = Config.from_yaml(args.config)
     executor = MultipleTargetsExecutor(config=config)
     executor.execute()
+    logger.info("Execution finished!")
 
 
-def cli() -> None:
-    parser = ArgumentParser()
+def profiles(args: Args) -> None:
+    """
+    Method to invoke when `profiles` is executed.
+    """
+
+    config = Config.from_yaml(args.config)
+    executor = ProfilesExecutor(profiles_root_path=config.selenium.profiles_root)
+    executor.print_profiles(output_format=args.format)
+
+
+def add_common_flags(parser: ArgumentParser) -> None:
+    """
+    Add common flags to a generic parser.
+
+    Flags provided:
+    * -c, --config
+    * -v, --verbose
+    """
+
     parser.add_argument(
+        "-c",
         "--config",
         type=Path,
         default=DEFAULT_CONFIG_PATH,
@@ -35,21 +74,80 @@ def cli() -> None:
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="Increase verbosity"
     )
-    args = Args(**vars(parser.parse_args()))
 
-    # handle verbosity
+
+def add_run_flags(parser: ArgumentParser) -> None:
+    """
+    Add `run` flags to a generic parser.
+
+    Flags provided: no one.
+    """
+
+    parser.set_defaults(func=run)
+
+
+def add_profiles_flags(parser: ArgumentParser) -> None:
+    """
+    Add `profiles` flags to a generic parser.
+
+    Flags provided:
+    * -f, --format
+    """
+
+    parser.add_argument(
+        "-f",
+        "--format",
+        default=OutputFormat.LIST.value,
+        choices=[v.value for v in list(OutputFormat)],
+        help=f"Output format for profiles. Defaults to {OutputFormat.LIST.value}",
+    )
+    parser.set_defaults(func=profiles)
+
+
+def cli() -> None:
+    # Construct the base parser
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(required=True, help="Available subcommands.")
+
+    # Construct the `run` parser
+    run_parser = subparsers.add_parser(
+        name="run",
+        help="Run AutoMSR on profiles specified in the config file, then exit.",
+    )
+    add_common_flags(parser=run_parser)
+    add_run_flags(parser=run_parser)
+
+    # Construct the `profiles` parser
+    profiles_parser = subparsers.add_parser(
+        name="profiles",
+        help="Retrieve the Chrome profiles found in the local machine with their full path.",
+    )
+    add_common_flags(parser=profiles_parser)
+    add_profiles_flags(parser=profiles_parser)
+
+    # Parse arguments
+    try:
+        raw_args = vars(parser.parse_args())
+    except TypeError:
+        parser.print_help()
+        sys.exit(1)
+    args = Args(**raw_args)
+
+    # Handle verbosity
     if args.verbose:
         level = logging.DEBUG
     else:
         level = logging.INFO
     logging.basicConfig(level=level)
 
-    # execute main functionality
-    main(args=args)
-
-
-if __name__ == "__main__":
+    # Set selenium and urllib3 logging level to WARNING,
+    # regardless of the tool's selected logging level
     logging.getLogger("selenium").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
+    # Execute subparser functionality
+    args.func(args)
+
+
+if __name__ == "__main__":
     cli()
